86bd69d29dcc8d79d536d3314d2d0d2e88390710
daniellimzj <daniellimzijie@gmail.com>
2020-08-16 21:38:14 +0800

    Add correct tag

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,5 @@
 public class Duke {
     public static void main(String[] args) {
-
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        */
-
         System.out.println("_________________________________________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
