3ebeeb5daa2af546588a07beb7235d0931fb276d
daniellimzj <daniellimzijie@gmail.com>
2020-08-23 18:48:47 +0800

    Add greet, echo, exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        System.out.println("_________________________________________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("_________________________________________________________________________________________");
+
+        String border = "==================================================";
+        System.out.println(border);
+        System.out.println(" _________________________ ");
+        System.out.println("| Hello! I'm Duke         |");
+        System.out.println("| What can I do for you?  |");
+        System.out.println("| ________________________|");
+        System.out.println("|/");
+        System.out.println(border);
+
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            System.out.println(border);
+            System.out.println(line);
+            System.out.println(border);
+            line = in.nextLine();
+        }
+
+        System.out.println(border);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_________________________________________________________________________________________");
+        System.out.println(border);
     }
 }
