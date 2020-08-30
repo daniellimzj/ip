f62de695ec6664d88940c4cc555e821d429291bb
daniellimzj <daniellimzijie@gmail.com>
2020-08-30 14:07:42 +0800

    Refactor methods for adding and doing Tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,52 +16,77 @@ public class Duke {
                 printList(tasks, border);
 
             } else if (line.startsWith("done ")) {
-                int taskNumber = Integer.parseInt(line.substring(5)) - 1;
-                // Some basic error checking
-                if (taskNumber > Task.taskCount - 1) {
-                    break;
-                }
-                tasks[taskNumber].setIsDone(true);
-                System.out.println(border);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks[taskNumber]);
-                System.out.println(border);
+                int taskNumber = markTaskDone(tasks, line);
+                printDoneTaskMessage(tasks[taskNumber], border);
 
             } else if (line.startsWith("todo")) {
-                String todo = line.substring(5);
-                tasks[Task.taskCount] = new ToDo(todo);
-                System.out.println(border);
-                System.out.println("added " + todo);
-                System.out.println(border);
+                addToDo(tasks, line);
+                printAddTaskMessage(tasks[Task.taskCount - 1], border);
 
             } else if (line.startsWith("deadline")) {
-                String deadline = line.substring(9);
-                int indexOfSlash = deadline.indexOf("/by");
-                String description = deadline.substring(0, indexOfSlash).trim();
-                String by = deadline.substring(indexOfSlash + 4).trim();
-                tasks[Task.taskCount] = new Deadline(description, by);
-                System.out.println(border);
-                System.out.println("added " + deadline);
-                System.out.println(border);
+                addDeadline(tasks, line);
+                printAddTaskMessage(tasks[Task.taskCount - 1], border);
 
 
             } else if (line.startsWith("event")) {
-                String event = line.substring(6);
-                int indexOfSlash = event.indexOf("/at");
-                String description = event.substring(0, indexOfSlash).trim();
-                String at = event.substring(indexOfSlash + 4).trim();
-                tasks[Task.taskCount] = new Event(description, at);
-                System.out.println(border);
-                System.out.println("added " + event);
-                System.out.println(border);
+                addEvent(tasks, line);
+                printAddTaskMessage(tasks[Task.taskCount - 1], border);
 
             } else {
-                System.out.println ("Unknown command!");
+                printErrorMessage(border);
             }
 
             line = in.nextLine();
         }
         printByeMessage(border);
+    }
+
+    private static int markTaskDone(Task[] tasks, String line) {
+        int taskNumber = Integer.parseInt(line.substring(5)) - 1;
+        tasks[taskNumber].setIsDone(true);
+        return taskNumber;
+    }
+
+    private static void addEvent(Task[] tasks, String line) {
+        String event = line.substring(6);
+        int indexOfSlash = event.indexOf("/at");
+        String description = event.substring(0, indexOfSlash).trim();
+        String at = event.substring(indexOfSlash + 4).trim();
+        tasks[Task.taskCount] = new Event(description, at);
+    }
+
+    private static void addDeadline(Task[] tasks, String line) {
+        String deadline = line.substring(9);
+        int indexOfSlash = deadline.indexOf("/by");
+        String description = deadline.substring(0, indexOfSlash).trim();
+        String by = deadline.substring(indexOfSlash + 4).trim();
+        tasks[Task.taskCount] = new Deadline(description, by);
+    }
+
+    private static void addToDo(Task[] tasks, String line) {
+        String todo = line.substring(5);
+        tasks[Task.taskCount] = new ToDo(todo);
+    }
+
+    private static void printDoneTaskMessage(Task task, String border) {
+        System.out.println(border);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task);
+        System.out.println(border);
+    }
+
+    private static void printErrorMessage(String border) {
+        System.out.println(border);
+        System.out.println ("Unknown command!");
+        System.out.println(border);
+    }
+
+    private static void printAddTaskMessage(Task task, String border) {
+        System.out.println(border);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + task);
+        System.out.printf("Now you have %d tasks in the list.\n", Task.taskCount);
+        System.out.println(border);
     }
 
     private static void printWelcomeMessage(String border) {
@@ -82,8 +107,8 @@ public class Duke {
 
     private static void printList(Task[] tasks, String border) {
         System.out.println(border);
-        for (int i = 0; i < Task.taskCount; i++) {
-            System.out.print((i + 1) + ".");
+        for (int i = 1; i <= Task.taskCount; i++) {
+            System.out.print((i) + ".");
             System.out.println(tasks[i]);
         }
         System.out.println(border);
